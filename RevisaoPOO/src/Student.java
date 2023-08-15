8adef445d0755923aa061bb7a6dae22316890ae7
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-08-14 21:00:08 -0300

    Atualizado - aula 14/08/2023

--- RevisaoPOO/src/Student.java
+++ RevisaoPOO/src/Student.java
@@ -1,8 +1,34 @@
 
 public class Student extends User{
 
-	public Student(int id) {
+	private int enrollment;
+	private String course;
+	private short semester;
+	
+	public Student(int id, int enrollment) {
 		super(id);
+		this.enrollment = enrollment;
 	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public short getSemester() {
+		return semester;
+	}
+
+	public void setSemester(short semester) {
+		this.semester = semester;
+	}
+
+	public int getEnrollment() {
+		return enrollment;
+	}
+	
 	
 }
