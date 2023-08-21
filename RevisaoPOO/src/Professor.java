04327fcda9cc906b6126bf7a255611f699495651
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-08-21 08:04:35 -0300

    Atualizado - aula 21/08/2023

--- RevisaoPOO/src/Professor.java
+++ RevisaoPOO/src/Professor.java
@@ -1,6 +1,33 @@
 
-public class Professor {
+public class Professor extends User{
 	private String siape;
 	private String level;
 	private String area;
+	
+	public Professor(int id, String siape) {
+		super(id);
+		this.siape = siape;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getSiape() {
+		return siape;
+	}
+	
+	
 }
