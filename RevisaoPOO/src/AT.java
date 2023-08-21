13f47d19fe1bb04a8e1869738db18c38ac2f3309
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-08-21 08:10:03 -0300

    Atualizado - aula 21/08/2023

--- RevisaoPOO/src/AT.java
+++ RevisaoPOO/src/AT.java
@@ -1,4 +1,42 @@
 
-public class AT {
+public class AT extends User{
+	private String siape;
+	private String level;
+	private String area;
+	private String role;
+	
+	public AT(int id, String siape) {
+		super(id);
+		this.siape = siape;
+	}
 
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getSiape() {
+		return siape;
+	}
+	
+	
 }
