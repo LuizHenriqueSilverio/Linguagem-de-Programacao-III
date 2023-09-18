02fe90d3dc557ecd8f3641201f8da4ee9eeacb47
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-09-18 07:17:49 -0300

    Atualização - aula 18/09/2023

---
+++ CorrecaoImpostosII/src/view/Main.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.Tax;
+import model.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tax ipi = new Tax("IPI", "Imposto Prod. Industrializados", 8);
+		Tax icms = new Tax("ICMS", "Imposto Circ. Mercadorias", 7);
+
+	}
+
+}
