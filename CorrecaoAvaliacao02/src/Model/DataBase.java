72e1405eb1c8d87af34badfbccfc8b0aef2e6492
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-11-13 08:21:55 -0300

    Atualizado - aula 13/11/2023

--- CorrecaoAvaliacao02/src/Model/DataBase.java
+++ CorrecaoAvaliacao02/src/Model/DataBase.java
@@ -8,12 +8,25 @@ public class DataBase {
 	public static Map<String, Employee> data = new HashMap<>();
 	
 	static {
-		data.put("maria", new Analyst("Maria Isabel", 4800.50));
-		data.put("jose", new Analyst("José Pedro", 2800.00));
-		data.put("pedro", new Architect("Pedro H.", 8800.00, 3, 2));
+		
+		Employee maria = new Analyst("Maria Isabel", 4800.50);
+		maria.setExtraHours(3);
+		maria.setPerExtraHourValue(100);
+		
+		Employee jose = new Analyst("José Pedro", 2800.00);
+		jose.setExtraHours(6);
+		jose.setPerExtraHourValue(100);
+		
+		Employee pedro = new Architect("Pedro H.", 8800.00, 3, 2);
+		pedro.setExtraHours(4);
+		pedro.setPerExtraHourValue(200);
+		
+		data.put("maria", maria);
+		data.put("jose", jose);
+		data.put("pedro", pedro);
 		data.put("joao", new Architect("João H.", 7800.00, 1, 2));
 		data.put("paula", new TechLead("Paula M.", 10000.00, 7, 5));
-		data.put("seila", new Manager("Seila X.", 12000.00, 8, 1, 2, 10));
+		data.put("seila", new Manager("Seila X.", 12000.00, 8, 1, 2, 800));
 	}
 	
 	public static Employee get(String pk) {
