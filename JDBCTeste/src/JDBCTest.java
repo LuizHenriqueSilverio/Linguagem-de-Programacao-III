c008b89b9e1625650d677d45694d40421fbeb668
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-12-04 10:41:14 -0300

    Atualizado - aula 04/12/2023

--- JDBCTeste/src/JDBCTest.java
+++ JDBCTeste/src/JDBCTest.java
@@ -6,15 +6,22 @@ import model.dao.UserDAO;
 public class JDBCTest {
 
 	public static void main(String[] args) throws ModelException {
-		User emerson = new User();
-		
-		emerson.setName("Emerson Teste DAO");
-		emerson.setGender("M");
-		emerson.setEmail("emerson@mail.com");
+		/*
+		User luiz = new User(6);
+		luiz.setName("Luiz Henrique");
+		luiz.setGender("M");
+		luiz.setEmail("luiz@mail.com");
 		
 		UserDAO userDAO = new MySQLUserDAO();
-		userDAO.save(emerson);
+		userDAO.save(luiz);
+		//userDAO.update(luiz);
+		*/
 		
+		UserDAO userDAO = new MySQLUserDAO();
+		
+		for(User user: userDAO.listAll()) {
+			System.out.println(user.getName() + " - " + user.getEmail());
+		}
 		
 	}
 	
