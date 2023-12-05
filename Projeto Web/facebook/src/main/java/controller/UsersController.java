5fcfccdfd1df11f1c5ae9bc27e111623f91e9408
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-12-05 10:43:33 -0300

    Atualizado - aula 05/12/2023

--- Projeto Web/facebook/src/main/java/controller/UsersController.java
+++ Projeto Web/facebook/src/main/java/controller/UsersController.java
@@ -1,5 +1,9 @@
 package controller;
 
-public class UsersController {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {""})
+public class UsersController extends HttpServlet{
 
 }
