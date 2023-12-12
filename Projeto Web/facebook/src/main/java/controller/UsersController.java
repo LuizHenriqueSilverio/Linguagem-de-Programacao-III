1782fabade0d15ab5f8aa33dbc12295f45950692
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-12-12 10:17:12 -0300

    Atualizado - aula 12/12/2023

--- Projeto Web/facebook/src/main/java/controller/UsersController.java
+++ Projeto Web/facebook/src/main/java/controller/UsersController.java
@@ -16,7 +16,7 @@ import model.dao.DAOFactory;
 import model.dao.UserDAO;
 
 //Rotas
-@WebServlet(urlPatterns = {"", "/save", "/user/update"})
+@WebServlet(urlPatterns = {"", "/save", "/user/update", "/user/delete"})
 public class UsersController extends HttpServlet{
 	
 	@Override
@@ -38,19 +38,35 @@ public class UsersController extends HttpServlet{
 			}
 			
 			case "/facebook/save": {
-				insertUser(req);
+				
+				String userId = req.getParameter("user-id");
+				
+				if(userId.equals("")) {
+					insertUser(req);
+				}else {
+					updateUser(req);
+				}
+				
 				resp.sendRedirect("/facebook");
 				
 				break;
 			}
 			
-			case "/user/update": {
+			case "/facebook/user/update": {
 				
 				loadUser(req);
 				
 				// Direcionar a chamada para a form_user.jsp
-				RequestDispatcher rd = req.getRequestDispatcher("form_user.jsp");
+				RequestDispatcher rd = req.getRequestDispatcher("/form_user.jsp");
 				rd.forward(req, resp);
+				
+				break;
+			}
+			
+			case "/facebook/user/delete": {
+				
+				deleteUser(req);
+				resp.sendRedirect("/facebook");
 				
 				break;
 			}
@@ -60,6 +76,46 @@ public class UsersController extends HttpServlet{
 				writer.append("404 recurso não encontrado: " + uri);
 				break;
 		}
+	}
+
+	private void deleteUser(HttpServletRequest req) {
+		String userId = req.getParameter("userId");
+		
+		User user = new User(Integer.parseInt(userId));
+		
+		UserDAO dao = DAOFactory.createDAO(UserDAO.class);
+		
+		try {
+			dao.delete(user);
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void updateUser(HttpServletRequest req) {
+		// Pegar os dados do formulário
+		String userId = req.getParameter("user-id");
+		String name = req.getParameter("user-name");
+		String gender = req.getParameter("user-gender");
+		String email = req.getParameter("email");
+				
+		// Criar um objeto do tipo User e setar os dados
+		User user = new User(Integer.parseInt(userId));
+		user.setName(name);
+		user.setGender(gender);
+		user.setEmail(email);
+				
+		// Criar um DAO e salvar o objeto
+		UserDAO dao = DAOFactory.createDAO(UserDAO.class);
+				
+		try {
+			dao.update(user);
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	private void loadUser(HttpServletRequest req) {
